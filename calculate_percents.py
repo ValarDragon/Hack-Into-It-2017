202ec45f4713f115049f7a65b2c41b22b0d0f7d6
Riley Lyman <rileylyman@berkeley.edu>
2017-09-15 23:59:52 -0700

    Started N percent calculations

---
+++ calculate_percents.py
@@ -0,0 +1,17 @@
+
+
+def possible_savings(amount_saved, daily_savings, days_until_grad):
+
+    sum_daily_savings = 0
+    for i in range(days_until_grad):
+        sum_daily_savings += daily_savings
+
+    return amount_saved + sum_daily_savings
+
+def income_bracket(max, min):
+
+    
+
+def money_needed(income_bracket, max_money_needed, min_money_needed):
+
+    return income_bracket(max_money_needed, min_money_needed)
