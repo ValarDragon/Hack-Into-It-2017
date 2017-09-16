68a136d4f46b7bca69ed9fa08e5a55441d94458f
Riley Lyman <rileylyman@berkeley.edu>
2017-09-16 10:16:07 -0700

    Fixed update function for manager

--- Manager.py
+++ Manager.py
@@ -7,7 +7,7 @@ class Manager:
     def __init__(self,daily_savings,
         days_until_grad, income_bracket,amount_saved =0):
 
-        self.User = Calculator(amount_saved,daily_savings
+        self.User = Calculator(amount_saved,daily_savings,
             days_until_grad, income_bracket)
 
         self.NeedList = GoalList([],
@@ -20,5 +20,5 @@ class Manager:
 
         self.NeedList.goal_list_percent = self.User.calculate_need_percent()
         self.WantList.goal_list_percent = 1 - self.User.calculate_need_percent()
-        self.NeedList.make_payment(daily_savings)
-        self.WantList.make_payment(daily_savings)
+        self.NeedList.make_payment(self.User.daily_savings)
+        self.WantList.make_payment(self.User.daily_savings)
